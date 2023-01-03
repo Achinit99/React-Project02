cf254797dc72c63f7b26ccbe9911c64382e0b2e0
Achinit99 <achinit720@gmail.com>
2023-01-03 21:34:35 +0530

    change text color
    Take good care of this => function Using

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
-  return <div className="App"></div>;
+  const [textColor, settextColor] = useState("green");
+
+  return (
+    <div className="App">
+      <button
+        onClick={() => {
+          settextColor(textColor === "green" ? "red" : "green");
+        }}
+      >
+        Show/Hide
+      </button>
+      <h1 style={{ color: textColor }}> Hi My Name is Achini</h1>
+    </div>
+  );
 }
 
 export default App;
