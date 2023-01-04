89de69515e47f0528db1e4f540ef50ffd7abcbc5
Achinit99 <achinit720@gmail.com>
2023-01-04 17:50:08 +0530

    add file

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
-import "./App.css";
-import { BrowserRouter as Router } from "react-router-dom";
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import { About } from "./pages/About";
+import { Home } from "./pages/Home";
+import { Contact } from "./pages/Contact";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <>
+      <nav>
+        <ul>
+          <li>
+            <link to="/">Home</link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/About" element={<About />} />
+        <Route path="/Contact" element={<Contact />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
---
+++ src/pages/Book.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export function Book() {
+  return <h1>Book</h1>;
+}
