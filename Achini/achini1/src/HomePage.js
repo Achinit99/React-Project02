603e0e4f1bba684c6f46ed3768a336b00e549564
Achinit99 <achinit720@gmail.com>
2023-03-01 00:40:32 +0530

    use router

---
+++ Achini/achini1/src/HomePage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function HomePage() {
+  return (
+    <div>
+      <h1>Welcome to My Website</h1>
+      <p>Here you will find information about our products and services.</p>
+    </div>
+  );
+}
+
+export default HomePage;
+
