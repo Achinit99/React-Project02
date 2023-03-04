caabfe25c3255a9df28b29b6fe188a46a6822d52
Achinit99 <achinit720@gmail.com>
2023-03-05 00:00:23 +0530

    Add pages in navbar

---
+++ Achini/achini1/src/components/MenuData.js
@@ -0,0 +1,32 @@
+export const MenuData = [
+  {
+    title: "Home",
+    url: "#",
+    cName: "nav-link",
+    icon: "fa-solid fa-house-user",
+  },
+  {
+    title: "About",
+    url: "#",
+    cName: "nav-link",
+    icon: "fa-solid fa-house-user",
+  },
+  {
+    title: "Home",
+    url: "#",
+    cName: "nav-link",
+    icon: "fa-solid fa-house-user",
+  },
+  {
+    title: "Home",
+    url: "#",
+    cName: "nav-link",
+    icon: "fa-solid fa-house-user",
+  },
+  {
+    title: "Home",
+    url: "#",
+    cName: "nav-link",
+    icon: "fa-solid fa-house-user",
+  },
+];
