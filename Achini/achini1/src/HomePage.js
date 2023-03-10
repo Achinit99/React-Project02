062d6c8220da3ecc825388e10c8caa6785ce8bbc
Achinit99 <achinit720@gmail.com>
2023-03-10 23:31:39 +0530

    edit nav bar

--- Achini/achini1/src/HomePage.js
+++ Achini/achini1/src/HomePage.js
@@ -1,13 +1,15 @@
-import React from 'react';
+import React from "react";
 
 function HomePage() {
   return (
     <div>
       <h1>Welcome to My Website</h1>
-      <p>Here you will find information about our products and services.</p>
+      <p>
+        Here you will find information about our products and services. @ Achini
+        Kawshalya Thathsarani
+      </p>
     </div>
   );
 }
 
 export default HomePage;
-
