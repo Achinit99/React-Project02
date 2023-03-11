662d4a359e6fb3677809a064cd361ad3ab5ef7bd
Achinit99 <achinit720@gmail.com>
2023-03-11 23:03:00 +0530

    Add menu icon class

--- Achini/achini1/src/components/Navbar.js
+++ Achini/achini1/src/components/Navbar.js
@@ -9,6 +9,9 @@ function Navbar() {
         <h1 className="logo">
           React <i className="fab fa-react"></i>
         </h1>
+        <div className="menu-icons">
+          <i className="fas fa-bars"></i>
+        </div>
         <ul className="nav-menu">
           {MenuData.map((item, index) => {
             return (
